2362e8c43601942cf98c095d318a4321ecebc091
Steven Realpe <nutritionmegastorepalmira@gmail.com>
2025-11-02 10:47:07 -0500

    fix: Cambiar importación de MDB UI Kit a versión gratuita en styles.scss 🎨✨

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
-import 'mdb-ui-kit/css/mdb.min.css';
 import './scss/styles.scss';
 import App from './App.jsx'
 
